04fb7e7a325101f4df3b7d5bae3606f7bf5c462a
Arham Jarif <arhamjarif007@gmail.com>
2026-07-06 00:37:42 +0600

    Introduce JSON packet protocol and username registration

--- client-1.py
+++ client-1.py
@@ -1,16 +1,24 @@
-import socket, threading
+import socket, threading,json
 
 host = "127.0.0.1"
 port = 6000
 def recv_check(client: socket.socket):
     while True:
-        message = client.recv(1024)
-        print(f'Received: {message.decode()}')
+        incoming_transmission = client.recv(1024)
+        incoming_packet = json.loads(incoming_transmission.decode())
+        if incoming_packet['type'] == 'public':
+            print(f"{incoming_packet['sender']}: {incoming_packet['message']}")
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as client:
     client.connect((host,port))
     print('Connected to server.\n')
+    username = input('Enter username: ')
+    login_packet = {'type':'login','username':username}
+    client.send(json.dumps(login_packet).encode())
     receive_thread = threading.Thread(target=recv_check,args=(client,))
     receive_thread.start()
+
     while True:
-        client.send(input('Enter Message:\n').encode())
+        public_message = input('Enter Message:\n')
+        public_packet = {'type':'public','message':public_message}
+        client.send(json.dumps(public_packet).encode())
         
--- server.py
+++ server.py
@@ -1,25 +1,29 @@
-import socket,threading
+import socket,threading,json
 
 host = "127.0.0.1"
 port = 6000
-def handle_client(conn: socket.socket, connections:list):
+def handle_client(conn: socket.socket, connections:dict):
     with conn:
         while True:
-            message = conn.recv(1024)
-            print(f'Received:\n{message.decode()}')
-            for connection in connections:
-                if conn != connection:
-                    connection.send(message)
+            incoming_transmission = conn.recv(1024)
+            incoming_packet = json.loads(incoming_transmission.decode())
+            if incoming_packet['type'] == 'login':
+                connections[conn] = incoming_packet['username']
+            elif incoming_packet['type'] == 'public':
+                outgoing_packet = {'type': 'public', 'sender':connections[conn],'message':incoming_packet['message']}
+                outgoing_transmission = json.dumps(outgoing_packet).encode()
+                for connection in connections:
+                    if conn != connection:
+                        connection.send(outgoing_transmission)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
     server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
     server.bind((host,port))
     server.listen()
     print('Server started\nWaiting for connection...\n')
-    connections = []
+    connections = {}
     while True:
         conn, addr = server.accept()
         print(f'{addr} has successfully connected...\n')
-        connections.append(conn)
         worker_thread = threading.Thread(target=handle_client,args=(conn, connections))
         worker_thread.start()

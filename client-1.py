ab57662502747042ac89a26e94e06dbc69f0f474
Arham Jarif <arhamjarif007@gmail.com>
2026-07-05 23:45:53 +0600

    Implement public chat message broadcasting

--- client-1.py
+++ client-1.py
@@ -1,11 +1,16 @@
-import socket
+import socket, threading
 
 host = "127.0.0.1"
 port = 6000
-
+def recv_check(client: socket.socket):
+    while True:
+        message = client.recv(1024)
+        print(f'Received: {message.decode()}')
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as client:
     client.connect((host,port))
     print('Connected to server.\n')
+    receive_thread = threading.Thread(target=recv_check,args=(client,))
+    receive_thread.start()
     while True:
         client.send(input('Enter Message:\n').encode())
-        print(f'Recieved:\n{client.recv(1024).decode()}')
+        
--- server.py
+++ server.py
@@ -2,19 +2,24 @@ import socket,threading
 
 host = "127.0.0.1"
 port = 6000
-def handle_client(conn: socket.socket):
+def handle_client(conn: socket.socket, connections:list):
     with conn:
         while True:
-            print(f'Recieved:\n{conn.recv(1024).decode()}')
-            conn.send(input('Enter Message:\n').encode())
+            message = conn.recv(1024)
+            print(f'Received:\n{message.decode()}')
+            for connection in connections:
+                if conn != connection:
+                    connection.send(message)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
     server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
     server.bind((host,port))
     server.listen()
     print('Server started\nWaiting for connection...\n')
+    connections = []
     while True:
         conn, addr = server.accept()
         print(f'{addr} has successfully connected...\n')
-        worker_thread = threading.Thread(target=handle_client,args=(conn,))
+        connections.append(conn)
+        worker_thread = threading.Thread(target=handle_client,args=(conn, connections))
         worker_thread.start()
